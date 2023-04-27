3001f04f4d2f20edabb65c99267b980cb9eb7aa9
dserrano02 <116206499+dserrano02@users.noreply.github.com>
2023-04-27 11:09:40 -0700

    I made a script
    I made a script

---
+++ ScriptingProjectSerranoP4/Assets/Scripts/Enumerations.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumScript : MonoBehaviour
+{
+    enum Direction { North, East, South, West };
+
+    void Start()
+    {
+        Direction myDirection;
+
+        myDirection = Direction.North;
+        Debug.Log(myDirection);
+        Debug.Log(ReverseDirection(myDirection));
+    }
+
+    Direction ReverseDirection(Direction dir)
+    {
+        if (dir == Direction.North)
+            dir = Direction.South;
+        else if (dir == Direction.South)
+            dir = Direction.North;
+        else if (dir == Direction.East)
+            dir = Direction.West;
+        else if (dir == Direction.West)
+            dir = Direction.East;
+
+        return dir;
+    }
+}
